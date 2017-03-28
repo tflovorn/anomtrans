d1d715fc5cb9d4e73ff51566da8e3c134cea0bf3
Tim Lovorn <tflovorn@austin.utexas.edu>
2017-03-28 14:18:40 -0500

    improve plot_series formatting

--- pyanomtrans/plot_series.py
+++ pyanomtrans/plot_series.py
@@ -4,11 +4,19 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_series(plot_path, val):
-    xs = range(len(val))
-    plt.plot(xs, val, 'ko')
+def plot_series(plot_path, ys, xs=None, xlabel=None, ylabel=None):
+    if xs is None:
+        xs = range(len(ys))
+
+    plt.plot(xs, ys, 'ko')
 
     plt.axhline(0.0, color='k', linestyle='-')
+
+    if xlabel is not None:
+        plt.xlabel(xlabel)
+
+    if ylabel is not None:
+        plt.ylabel(ylabel)
 
     plt.savefig("{}.png".format(plot_path), bbox_inches='tight', dpi=500)
     plt.clf()
@@ -32,15 +40,30 @@ def _main():
     with open(fpath, 'r') as fp:
         fdata = json.load(fp)
 
-    all_data = {}
+    series_data = {}
     for key, val in fdata.items():
         if _ignore_key(key):
             continue
 
-        all_data[key] = val
+        series_data[key] = val
 
-    for key, val in all_data.items():
-        plot_series("{}_{}".format(args.prefix, key), val)
+    special_keys = {
+            '_series_Hall_conductivity': {
+                    'xs': 'mus',
+                    'xlabel': "$\\mu$",
+                    'ylabel': "$\\sigma^{Hall}_{xy}$"
+            }
+    }
+
+    for key, ys in series_data.items():
+        if key in special_keys:
+            xs = fdata[special_keys[key]['xs']]
+            xlabel = special_keys[key]['xlabel']
+            ylabel = special_keys[key]['ylabel']
+
+            plot_series("{}_{}".format(args.prefix, key), ys, xs=xs, xlabel=xlabel, ylabel=ylabel)
+        else:
+            plot_series("{}_{}".format(args.prefix, key), ys)
 
 if __name__ == '__main__':
     _main()
