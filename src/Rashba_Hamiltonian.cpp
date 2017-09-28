69a22ad26dc9018bc2e856be3267df80bf211367
Tim Lovorn <tflovorn@austin.utexas.edu>
2017-09-28 17:07:25 -0500

    add version of Rashba Hamiltonian with magnetic moment

--- src/Rashba_Hamiltonian.cpp
+++ src/Rashba_Hamiltonian.cpp
@@ -21,7 +21,7 @@ double Rashba_Hamiltonian::energy(kmComps<2> ikm_comps) const {
 
   double diag_term = 2.0 * t0 * (2.0 - std::cos(kx_a) - std::cos(ky_a));
 
-  double rashba_term = 2.0 * tr * std::sqrt(std::pow(std::sin(kx_a), 2.0) +
+  double rashba_term = 2.0 * std::abs(tr) * std::sqrt(std::pow(std::sin(kx_a), 2.0) +
       std::pow(std::sin(ky_a), 2.0));
 
   if (m == 0) {
@@ -45,7 +45,8 @@ std::complex<double> u_component(kVals<2> k) {
     // (kx > 0, ky = 0).
     // Here we assume that k in reciprocal lattice coordinates
     // is restricted to [0, 1) x [0, 1).
-    // Exact comparison should be OK since 0.5 is exactly representable.
+    // Exact comparison should be OK since 0.5 is exactly representable
+    // and generated in km_at by division, not repeated addition.
     return std::complex<double>(1.0/std::sqrt(2.0));
   }
 
@@ -87,6 +88,26 @@ std::complex<double> Rashba_Hamiltonian::basis_component(const PetscInt ikm, con
   }
 }
 
+namespace {
+
+std::array<std::complex<double>, 2> gradient_impl(double kx_a, double ky_a,
+    double t0, double tr, std::complex<double> um, std::complex<double> vm,
+    std::complex<double> ump, std::complex<double> vmp) {
+  // Would prefer to use std::complex_literals for this, but not available in C++11.
+  // Not sure if this C++14 feature is supported in icpc16 - not given on feature page.
+  std::complex<double> i2(0.0, 2.0);
+
+  std::complex<double> vx = std::conj(um) * (2.0 * t0 * std::sin(kx_a) * ump + i2 * tr * std::cos(kx_a) * vmp)
+      + std::conj(vm) * (-i2 * tr * std::cos(kx_a) * ump + 2.0 * t0 * std::sin(kx_a) * vmp);
+
+  std::complex<double> vy = std::conj(um) * (2.0 * t0 * std::sin(ky_a) * ump + 2.0 * tr * std::cos(ky_a) * vmp)
+      + std::conj(vm) * (2.0 * tr * std::cos(ky_a) * ump + 2.0 * t0 * std::sin(ky_a) * vmp);
+
+  return {vx, vy};
+}
+
+} // namespace
+
 std::array<std::complex<double>, 2> Rashba_Hamiltonian::gradient(kmComps<2> ikm_comps, unsigned int mp) const {
   kmVals<2> km = km_at(kmb.Nk, ikm_comps);
   kVals<2> k = std::get<0>(km);
@@ -108,17 +129,160 @@ std::array<std::complex<double>, 2> Rashba_Hamiltonian::gradient(kmComps<2> ikm_
   std::complex<double> ump = basis_component(ikmp, 0);
   std::complex<double> vmp = basis_component(ikmp, 1);
 
-  // Would prefer to use std::complex_literals for this, but not available in C++11.
-  // Not sure if this C++14 feature is supported in icpc16 - not given on feature page.
-  std::complex<double> i2(0.0, 2.0);
+  return gradient_impl(kx_a, ky_a, t0, tr, um, vm, ump, vmp);
+}
 
-  std::complex<double> vx = std::conj(um) * (2.0 * t0 * std::sin(kx_a) * ump + i2 * tr * std::cos(kx_a) * vmp)
-      + std::conj(vm) * (-i2 * tr * std::cos(kx_a) * ump + 2.0 * t0 * std::sin(kx_a) * vmp);
+Rashba_magnetic_Hamiltonian::Rashba_magnetic_Hamiltonian(double _t0, double _tr, double _M, kmBasis<2> _kmb)
+  : t0(_t0), tr(_tr), M(_M), kmb(_kmb) {
+    assert(kmb.Nbands == 2);
+}
 
-  std::complex<double> vy = std::conj(um) * (2.0 * t0 * std::sin(ky_a) * ump + 2.0 * tr * std::cos(ky_a) * vmp)
-      + std::conj(vm) * (2.0 * tr * std::cos(ky_a) * ump + 2.0 * t0 * std::sin(ky_a) * vmp);
+std::complex<double> Rashba_magnetic_Hamiltonian::Hr(double kx_a, double ky_a) const {
+  return 2.0 * tr * (std::complex<double>(0.0, std::sin(kx_a))
+      + std::complex<double>(std::sin(ky_a), 0.0));
+}
 
-  return {vx, vy};
+double Rashba_magnetic_Hamiltonian::lambda(double kx_a, double ky_a) const {
+  return std::sqrt(std::norm(Hr(kx_a, ky_a)) + std::pow(M, 2.0));
+}
+
+double Rashba_magnetic_Hamiltonian::energy(kmComps<2> ikm_comps) const {
+  kmVals<2> km = km_at(kmb.Nk, ikm_comps);
+  kVals<2> k = std::get<0>(km);
+  unsigned int m = std::get<1>(km);
+
+  if (m > 1) {
+    throw std::invalid_argument("Rashba_magnetic_Hamiltonian is not defined for Nbands > 2");
+  }
+
+  double kx_a = 2.0*pi*k.at(0);
+  double ky_a = 2.0*pi*k.at(1);
+
+  double diag_term = 2.0 * t0 * (2.0 - std::cos(kx_a) - std::cos(ky_a));
+
+  if (m == 0) {
+    return diag_term + lambda(kx_a, ky_a);
+  } else {
+    return diag_term - lambda(kx_a, ky_a);
+  }
+}
+
+namespace {
+
+std::complex<double> magnetic_component_special(double M, unsigned int m, unsigned int i) {
+  if (M > 0.0) {
+    // M - lambda = 0 case.
+    if (m == 0) {
+      // |u_k^+>
+      if (i == 0) {
+        return std::complex<double>(0.0, -1.0);
+      } else {
+        return std::complex<double>(0.0, 0.0);
+      }
+    } else {
+      // |u_k^->
+      if (i == 0) {
+        return std::complex<double>(0.0, 0.0);
+      } else {
+        return std::complex<double>(0.0, 1.0);
+      }
+    }
+  } else {
+    // M + lambda = 0 case.
+    if (m == 0) {
+      // |u_k^+>
+      if (i == 0) {
+        return std::complex<double>(0.0, 0.0);
+      } else {
+        return std::complex<double>(0.0, -1.0);
+      }
+    } else {
+      // |u_k^->
+      if (i == 0) {
+        return std::complex<double>(0.0, -1.0);
+      } else {
+        return std::complex<double>(0.0, 0.0);
+      }
+    }
+  }
+}
+
+} // namespace
+
+std::complex<double> Rashba_magnetic_Hamiltonian::basis_component(const PetscInt ikm, const unsigned int i) const {
+  auto ikm_comps = kmb.decompose(ikm);
+
+  kmVals<2> km = km_at(kmb.Nk, ikm_comps);
+  kVals<2> k = std::get<0>(km);
+  unsigned int m = std::get<1>(km);
+
+  if (m > 1 or i > 1) {
+    throw std::invalid_argument("Rashba_magnetic_Hamiltonian is not defined for Nbands > 2");
+  }
+
+  if ((k.at(0) == 0.0 and k.at(1) == 0.0)
+      or (k.at(0) == 0.5 and k.at(1) == 0.5)) {
+    // At k = (0, 0) and k = (1/2, 1/2), the eigenvector components are not well-defined.
+    // Choose the values obtained by approaching along the path such that Hr/|Hr| = 1 
+    // with M != 0.
+    // Here we assume that k in reciprocal lattice coordinates
+    // is restricted to [0, 1) x [0, 1).
+    // Exact comparison should be OK since 0.5 is exactly representable
+    // and generated in km_at by division, not repeated addition.
+    return magnetic_component_special(M, m, i);
+  }
+
+  double kx_a = 2.0*pi*k.at(0);
+  double ky_a = 2.0*pi*k.at(1);
+
+  std::complex<double> ilit(0.0, 1.0);
+  double l = lambda(kx_a, ky_a);
+
+  if (m == 0) {
+    if (i == 0) {
+      // u_+
+      return -ilit * Hr(kx_a, ky_a) / std::sqrt(2.0 * l * (l - M));
+    } else {
+      // v_+
+      return -ilit * std::sqrt(l - M) / std::sqrt(2.0 * l);
+    }
+  } else {
+    if (i == 0) {
+      // u_-
+      return -ilit * Hr(kx_a, ky_a) / std::sqrt(2.0 * l * (l + M));
+    } else {
+      // v_-
+      return ilit * std::sqrt(l + M) / std::sqrt(2.0 * l);
+    }
+  }
+}
+
+std::array<std::complex<double>, 2> Rashba_magnetic_Hamiltonian::gradient(kmComps<2> ikm_comps, unsigned int mp) const {
+
+  kmVals<2> km = km_at(kmb.Nk, ikm_comps);
+  kVals<2> k = std::get<0>(km);
+  unsigned int m = std::get<1>(km);
+
+  if (m > 1 or mp > 1) {
+    throw std::invalid_argument("Rashba_magnetic_Hamiltonian is not defined for Nbands > 2");
+  }
+
+  double kx_a = 2.0*pi*k.at(0);
+  double ky_a = 2.0*pi*k.at(1);
+
+  PetscInt ikm = kmb.compose(ikm_comps);
+  auto ikmp_comps = std::make_tuple(std::get<0>(ikm_comps), mp);
+  PetscInt ikmp = kmb.compose(ikmp_comps);
+
+  // Gradient takes the same form as Rashba_Hamiltonian, since the M term added to
+  // the Hamiltonian is k-invariant.
+  // Need to use magnetic basis components, though.
+  std::complex<double> um = basis_component(ikm, 0);
+  std::complex<double> vm = basis_component(ikm, 1);
+  std::complex<double> ump = basis_component(ikmp, 0);
+  std::complex<double> vmp = basis_component(ikmp, 1);
+
+  return gradient_impl(kx_a, ky_a, t0, tr, um, vm, ump, vmp);
 }
 
 } // namespace anomtrans
--- src/Rashba_Hamiltonian.h
+++ src/Rashba_Hamiltonian.h
@@ -57,6 +57,63 @@ public:
   std::complex<double> basis_component(PetscInt ikm, unsigned int i) const;
 };
 
+/** @brief k-space Hamiltonian for square lattice (or higher-dimensional version of square
+ *         lattice) tight-binding model with Rashba coupling and magnetic moment.
+ *  @note The lattice spacing a is set to 1.
+ */
+class Rashba_magnetic_Hamiltonian {
+  /** @brief Calculate the Rashba term Hr(k) giving the (up, down) part of the Hamiltonian.
+   */
+  std::complex<double> Hr(double kx_a, double ky_a) const;
+
+  /** @brief Calculate lambda \equiv (|Hr|^2 + M^2)^(1/2).
+   */
+  double lambda(double kx_a, double ky_a) const;
+
+public:
+  /** @brief Nearest-neighbor orbital-preserving hopping amplitude.
+   */
+  const double t0;
+
+  /** @brief Nearest-neighbor Rashba hopping amplitude.
+   */
+  const double tr;
+
+  /** @brief Magnetization (in units of energy).
+   */
+  const double M;
+
+  /** @brief Discretization of (k, m) basis on which this Hamiltonian is defined.
+   *  @todo Would prefer to avoid copying kmb: it may be large.
+   *        Replace kmb copy with a shared pointer?
+   */
+  const kmBasis<2> kmb;
+
+  Rashba_magnetic_Hamiltonian(double _t0, double _tr, double _M, kmBasis<2> _kmb);
+
+  /** @brief Energy at (k,m): E_{km}.
+   */
+  double energy(kmComps<2> ikm_comps) const;
+
+  /** @brief Gradient of the Hamiltonian, evaluated in the eigenbasis;
+   *         equal to the covariant derivative of the Hamiltonain.
+   *         gradient(ikm, mp) = <k, m|grad_k H|k, mp>.
+   */
+  std::array<std::complex<double>, 2> gradient(kmComps<2> ikm_comps, unsigned int mp) const;
+
+  /** @brief Velocity at (k, m): v_{km} = dE_{km}/dk|_{k}.
+   *  @note By Hellmann-Feynman theorem, this is equal to gradient(km, m)
+   *         = <k, m|grad_k H|k, m>.
+   */
+  std::array<double, 2> velocity(kmComps<2> ikm_comps) const;
+
+  /** @brief Value of U_{im}(k), where U is the unitary matrix which diagonalizes
+   *         H(k), m is the eigenvalue index, and i is the component of the
+   *         initial basis (pseudo-atomic orbital or otherwise).
+   */
+  std::complex<double> basis_component(PetscInt ikm, unsigned int i) const;
+};
+
 } // namespace anomtrans
 
 #endif // ANOMTRANS_RASHBA_HAMILTONIAN_H
