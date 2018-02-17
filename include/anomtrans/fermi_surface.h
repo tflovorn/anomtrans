019f116cb5df72acda46ab8f3f447c97e40a2035
Tim Lovorn <tflovorn@austin.utexas.edu>
2018-02-17 17:26:26 -0600

    rewrite implementation of `find_nonzero_fs`
    Previous algorithm considered only intervals [L, H] where
    \delta(E_L, E_H) is not negligible. But the real intervals we want
    are [L, .., C, .., H] with n_C not negligible and \delta(E_L, E_C),
    \delta(E_C, E_H) not negligible. Replace the `find_nonzero_fs`
    algorithm with one which looks for this correct type of interval.
    The new algorithm is also conceptually simpler.
    
    The new algorithm produces DeltaBin results for Rashba_magnetic
    (64x64, beta = 80/t, num_fs = 4 * Nk(0)) which are visually identical
    to those produced by a direct FS binning procedure (see branch
    `fs_bin_direct`). It also produces DeltaGaussian results which
    are visually identical to those produced before `find_nonzero_fs`
    was introduced.

--- include/anomtrans/fermi_surface.h
+++ include/anomtrans/fermi_surface.h
@@ -19,15 +19,18 @@ namespace anomtrans {
 PetscReal get_n_threshold(const std::vector<PetscScalar> &n_all);
 
 /** @brief Iterate over `ikm` values to find those Fermi surfaces which have at least one value
- *         at which `abs(n_all.at(ikm)) > threshold`. Return a vector where the entries are
+ *         at which `abs(n_all.at(ikm)) > n_threshold`. Return a vector where the entries are
  *         `true` for `ikm` on such Fermi surfaces, and `false` otherwise.
+ *         The "Fermi surface" associated with point `ikm` is defined here as the set of points
+ *         `ikpmp` on which `\delta(ikm, ikmpm) > delta.threshold`.
  *  @param delta Delta function representation `delta(E1, E2)`. Must satisfy the following
  *               conditions:
  *               (1) `\delta(E1, E2) = \delta(E2, E1)`;
  *               (2) `\delta(E, E) != 0`;
- *               (3) For fixed `E1 <= E2`, `|\delta(E1, E2)|` is non-increasing with
+ *               (3) `\delta(E1, E2) >= 0`;
+ *               (4) For fixed `E1 <= E2`, `\delta(E1, E2)` is non-increasing with
  *               increasing `E2`.
- *               (4) `\delta(E1, E2)` is normalized such that
+ *               (5) `\delta(E1, E2)` is normalized such that
  *               `\int_{-\infty}^{\infty} dE1 \delta(E1, E2) = 1`
  *               and there exists a limit such that
  *               `\int_{-\infty}^{\infty} dE1 \delta(E1, E2) f(E1) = f(E2)`.
@@ -37,6 +40,13 @@ PetscReal get_n_threshold(const std::vector<PetscScalar> &n_all);
  *  @param n_all Vector of <n> values over all `ikm`.
  *  @param n_threshold Value below which `|n|` is considered to be vanishing.
  *  @note This function requires only O(`n_all.size()`) operations and memory.
+ *  @note TODO - parallelization strategy. Each thread starts `center` in a different place,
+ *        evenly distributed over the set of sorted indices.
+ *        `center` of the i'th thread occupies an interval [c^{begin}_i, c^{end}_i),
+ *        with c^{end}_i = c^{begin}_{i+1}, c^{begin}_{0} = 0,
+ *        and c^{end}_{N_threads - 1} = n_all.size().
+ *        `center` of the i'th thread initially takes the value c^{begin}_i, and iteration
+ *        on that thread stops when that `center` takes the value c^{end}_i.
  */
 template <typename Delta>
 boost::container::vector<bool> find_nonzero_fs(const Delta& delta, const SortResult& Ekm,
@@ -53,48 +63,86 @@ boost::container::vector<bool> find_nonzero_fs(const Delta& delta, const SortRes
   if (n_all.size() == 0) {
     return nonzero_fs;
   }
+  PetscInt end = n_all.size();
 
+  // Indices in the sorted sequence of energies, defining Fermi surface intervals.
+  // The algorithm is defined such that each of these indices never takes the same value
+  // twice, ensuring that `find_nonzero_fs` executes in time linearly proportional
+  // to `n_all.size()`.
   PetscInt low = 0; // `L`
   PetscInt high = 0; // `H`
-  PetscInt n_highest_seen = -1; // `N`
+  PetscInt center = 0; // `C`
 
-  // Special case: first point. Check n(0).
-  if (std::abs(n_all.at(Ekm.at(high).second)) > n_threshold) {
-    n_highest_seen = 0;
-    nonzero_fs.at(Ekm.at(0).second) = true;
+  // Initialization: find the first subinterval, in which `|n(C)| > n_threshold`,
+  // `\delta(E(L), E(C)) > delta.threshold` and either `L = 0` or
+  // `\delta(E(L-1), E(C)) < delta.threshold`, and `\delta(E(C), E(H)) > delta.threshold`
+  // and either `H = n_all.size() - 1` or `\delta(E(C), E(H+1)) < delta.threshold`.
+  while (std::abs(n_all.at(Ekm.at(center).second)) < n_threshold and center < end) {
+    center++;
+  }
+  if (center == end) {
+    return nonzero_fs;
   }
 
-  // General case: L <= H; all n values <= H have been examined.
-  while (high < static_cast<PetscInt>(n_all.size()) - 1) {
-    // Advance H.
+  nonzero_fs.at(Ekm.at(center).second) = true;
+  low = center;
+  high = center;
+  while (low > 0
+      and std::abs(delta(Ekm.at(low - 1).first, Ekm.at(center).first)) >= delta.threshold) {
+    low--;
+    nonzero_fs.at(Ekm.at(low).second) = true;
+  }
+  while (high < end - 1
+      and std::abs(delta(Ekm.at(center).first, Ekm.at(high + 1).first)) >= delta.threshold) {
     high++;
-
-    // Is delta(L, H) finite? If not, advance L until delta(L, H) is finite.
-    // N is less than H. As long as L <= N, L is on an active Fermi surface.
-    while (std::abs(delta(Ekm.at(low).first, Ekm.at(high).first)) < delta.threshold) {
-      if (low <= n_highest_seen) {
-        nonzero_fs.at(Ekm.at(low).second) = true;
-      }
-      low++;
-    }
-
-    // Is n(H) finite? If so, advance N to H.
-    if (std::abs(n_all.at(Ekm.at(high).second)) > n_threshold) {
-      n_highest_seen = high;
-    }
-
-    // Is L <= N? If so, L belongs to an active Fermi surface.
-    if (low <= n_highest_seen) {
-      nonzero_fs.at(Ekm.at(low).second) = true;
-    }
+    nonzero_fs.at(Ekm.at(high).second) = true;
   }
+  //PetscPrintf(PETSC_COMM_WORLD, "init complete, low = %d, center = %d, high = %d\n", low, center, high);
 
-  // Special case: final point. If L <= N, all remaining points are on an active Fermi surface.
-  if (low <= n_highest_seen) {
-    while (low < static_cast<PetscInt>(n_all.size())) {
-      nonzero_fs.at(Ekm.at(low).second) = true;
+  // Now we have a subinterval `[low, high]` with `low <= center <= high` and all points in the interval
+  // `[low, high]` are marked as being on an occupied Fermi surface.
+  // Now advance `center` util we find the next subinterval, defined in the same way as in
+  // the initialization step.
+  while (high < end - 1) {
+    PetscInt high_prev = high; // `H_0`
+
+    center++;
+    while (center < end and std::abs(n_all.at(Ekm.at(center).second)) < n_threshold) {
+      center++;
+    }
+    if (center == end) {
+      return nonzero_fs;
+    }
+
+    // TODO: can jump to `low = center` and move backwards instead if `center > high_prev`
+    // (since `\delta(E(C), E(H_0 - 1)) < delta.threshold` in this case).
+    while (std::abs(delta(Ekm.at(low).first, Ekm.at(center).first)) < delta.threshold) {
       low++;
     }
+
+    // Jump `high` ahead if center has passed it, so that moving it forward always moves it away from `center`.
+    if (high < center) {
+      high = center;
+    }
+    while (high < end - 1
+        and std::abs(delta(Ekm.at(center).first, Ekm.at(high + 1).first)) >= delta.threshold) {
+      high++;
+    }
+
+    // Mark the points in `[low, high]` which do not overlap with the previous subinterval.
+    // If `low > high_prev`, this is the full interval `[low, high]`.
+    // Otherwise this is the interval `(high_prev, high]` (which is empty if `high = high_prev`).
+    if (low > high_prev) {
+      for (PetscInt i = low; i <= high; i++) {
+        nonzero_fs.at(Ekm.at(i).second) = true;
+      }
+    } else {
+      for (PetscInt i = high_prev + 1; i <= high; i++) {
+        nonzero_fs.at(Ekm.at(i).second) = true;
+      }
+    }
+
+    //PetscPrintf(PETSC_COMM_WORLD, "advance complete, low = %d, center = %d, high = %d\n", low, center, high);
   }
 
   return nonzero_fs;
